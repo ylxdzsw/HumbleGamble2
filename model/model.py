c9146bd3a0ab03f536630e4bc38a46681a291c9f
ylxdzsw <ylxdzsw@gmail.com>
2018-02-06 21:48:04 +0800

    such trainable

--- model/model.py
+++ model/model.py
@@ -1,35 +1,37 @@
 import tensorflow as tf
 
+epsilon = tf.constant(1e-10)
+
 kline = tf.placeholder(dtype=tf.float32, shape=[None, 132, 10])
 depth = tf.placeholder(dtype=tf.float32, shape=[None, 5, 4])
 label = tf.placeholder(dtype=tf.float32, shape=[None, 5, 5])
 
 conv1 = tf.layers.conv1d(kline, filters=10, kernel_size=5, activation=tf.nn.leaky_relu, padding='valid')
 pool1 = tf.layers.max_pooling1d(conv1, 2, 2)
-conv2 = tf.layers.conv1d(pool1, filters=10, kernel_size=5, activation=tf.nn.leaky_relu)
+conv2 = tf.layers.conv1d(pool1, filters=10, kernel_size=5, activation=tf.nn.leaky_relu, padding='same')
 pool2 = tf.layers.max_pooling1d(conv2, 2, 2)
-conv3 = tf.layers.conv1d(pool2, filters=10, kernel_size=5, activation=tf.nn.leaky_relu)
+conv3 = tf.layers.conv1d(pool2, filters=10, kernel_size=5, activation=tf.nn.leaky_relu, padding='same')
 pool3 = tf.layers.max_pooling1d(conv3, 2, 2)
-conv4 = tf.layers.conv1d(pool3, filters=10, kernel_size=5, activation=tf.nn.leaky_relu)
+conv4 = tf.layers.conv1d(pool3, filters=10, kernel_size=5, activation=tf.nn.leaky_relu, padding='same')
 pool4 = tf.layers.max_pooling1d(conv4, 2, 2)
 
-kline_feature = tf.layers.dense(pool4, 10, activation=tf.nn.leaky_relu)
-depth_feature = tf.layers.dense(depth, 10, activation=tf.nn.leaky_relu)
+kline_feature = tf.layers.dense(tf.layers.flatten(pool4), 10, activation=tf.nn.leaky_relu)
+depth_feature = tf.layers.dense(tf.layers.flatten(depth), 10, activation=tf.nn.leaky_relu)
 
 cat = tf.concat([kline_feature, depth_feature], 1)
 fc1 = tf.layers.dense(cat, 10, activation=tf.nn.leaky_relu)
 fc2 = tf.layers.dense(fc1, 25)
-out = tf.nn.log_softmax(tf.reshape(fc2, [-1, 5, 5]))
+out = tf.nn.softmax(tf.reshape(fc2, [-1, 5, 5]))
 
-loss = tf.reduce_sum(- out * label)
-step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+loss = -tf.reduce_mean(label * tf.log(out + epsilon))
+step = tf.train.GradientDescentOptimizer(0.002).minimize(loss)
 
 saver = tf.train.Saver()
 
 sess = tf.Session()
 
 try:
-    saver.restore(sess, "/var/HumbleGamble2/model.ckpt")
+    saver.restore(sess, tf.train.latest_checkpoint("/var/HumbleGamble2/"))
 except:
     sess.run(tf.initializers.global_variables())
 
@@ -37,7 +39,7 @@ def predict(dkline, ddepth):
     return sess.run(out, feed_dict={ kline: dkline, depth: ddepth })
 
 def train(dkline, ddepth, dlabel):
-    return [sess.run([loss, step], feed_dict={ kline: dkline, depth: ddepth, label: dlabel })[0] for i in range(5)]
+    return sess.run([loss, step], feed_dict={ kline: dkline, depth: ddepth, label: dlabel })[0]
 
 def save():
     return saver.save(sess, "/var/HumbleGamble2/model.ckpt")
--- model/train.jl
+++ model/train.jl
@@ -43,7 +43,7 @@ function cherry_pick(ds)
     for i in 132:length(ds)-1 @when all(length(ds[j]) > 10 && ds[j][10] > 5 for j in i:i-4)
         kline, depth = scale_and_format(ds[i-131:i])
         sellp, buyp  = ds[i+1][[2, 4]]
-        push!(cherries, (kline, depth, buyp, sellp))
+        push!(cherries, (kline, depth, i, buyp, sellp))
     end
     cherries
 end
@@ -73,4 +73,94 @@ function scale_and_format(data)
     end
     
     kline, depth
+end
+
+function predict_all!(data)
+    klines = Array{f64}(256, 132, 10)
+    depths = Array{f64}(256, 5, 4)
+    for i in 0:256:length(data)-1
+        n = min(256, length(data) - i)
+        for j in 1:n
+            klines[j, :, :] = data[i+j][1]
+            depths[j, :, :] = data[i+j][2]
+        end
+        
+        pred = model.predict(klines, depths)
+        
+        for j in 1:n
+            pacc = accumulate(+, pred[j, :, :] .* .95 .+ .01, 2) # ensure a minimum possiblity
+            pacc[:, end] = 1 # ensure absolute 1
+            data[i+j] = (data[i+j]..., pacc)
+        end
+    end
+    data
+end
+
+function random_walk(data, labels=map(x->fill(0., 5, 5), 1:length(data)))
+    function walk(data)
+        decisions = []
+        vs = [ -35, -10, 0, 10, 35 ]
+        stock, balance = 3, 0
+        
+        for (kline, depth, i, buyp, sellp, pacc) in data
+            action = i == data[end][3] ? 3 : let r = rand()
+                findfirst(x->x>r, pacc[stock, :])
+            end
+            
+            if action > stock
+                balance += 100(vs[action] - vs[stock]) / buyp * 0.9995
+            elseif action < stock
+                balance -= 100(vs[stock] - vs[action]) / sellp
+            end
+            
+            push!(decisions, (stock, action))
+            stock = action
+        end
+        
+        balance, decisions[1:end-1]
+    end
+    
+    balances = []
+    
+    for i in 30:length(data) @when data[i][3] - data[i-29][3] == 29
+        records = [walk(data[i-29:i]) for n in 1:100]
+        mid = median(car.(records))
+        push!(balances, mid)
+        for (balance, decisions) in records
+            g = sign(balance - mid)
+            for (j, (stock, action)) in enumerate(decisions)
+                labels[i-30+j][stock, action] += g
+            end
+        end
+    end
+    
+    mean(balances), labels
+end
+
+function learn(data, labels)
+    loss = 0
+    
+    for i in 0:256:length(data)-1
+        n = min(256, length(data) - i)
+        
+        klines = Array{f32}(n, 132, 10)
+        depths = Array{f32}(n, 5, 4)
+        grads  = Array{f32}(n, 5, 5)
+        
+        for j in 1:n
+            klines[j, :, :] = data[i+j][1]
+            depths[j, :, :] = data[i+j][2]
+            grads[j, :, :] = labels[i+j]
+        end
+        
+        loss += model.train(klines, depths, grads) |> sum
+    end
+    
+    loss / length(data)
+end
+
+function rewalk!(data, labels)
+    data = map(x->x[1:end-1], data)
+    predict_all!(data)
+    random_walk(data, labels)
 end
